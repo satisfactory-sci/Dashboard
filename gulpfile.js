e345d5b947e75ee3a7295b12aa7a956bd3a4e88c
Ville Vainio <ville.vainio@aalto.fi>
2016-11-01 18:18:10 +0200

    Add production confs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var browserify = require("browserify")
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
 var babelify = require("babelify")
 
 gulp.task("build", function() {
@@ -18,11 +19,28 @@ gulp.task("build", function() {
     .pipe(gulp.dest('./public'));
 })
 
+gulp.task('product', function() {
+  var b = browserify({
+    entries: './src/index.js',
+    transform: ['babelify']
+  })
+  return b.bundle()
+  .pipe(source('build.js'))
+  .pipe(buffer())
+  .pipe(uglify())
+  .on('error', gutil.log)
+  .pipe(gulp.dest('./public/'));
+})
+
 gulp.task("watch", function() {
   gulp.watch('src/*.js', ['build'])
   gulp.watch('src/Components/*.js', ['build'])
 })
 
 gulp.task("default", function() {
-  gulp.start('build', 'watch')
+  if(!!gutil.env.production){
+    gulp.start('product')
+  }else{
+    gulp.start('build', 'watch')
+  }
 })
